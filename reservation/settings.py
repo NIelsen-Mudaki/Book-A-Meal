6be900860dccc42bcb8083b9085bff49c9d169e5
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-30 11:06:59 +0300

    add settings configurations

--- reservation/settings.py
+++ reservation/settings.py
@@ -90,12 +90,18 @@ WSGI_APPLICATION = 'reservation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES={
+
+        'default':{
+
+            'ENGINE':'django.db.backends.postgresql',
+            'NAME':config('DB_NAME'),
+            'USER':config('DB_USER'),
+            'PASSWORD':config('DB_PASSWORD'),
+            'HOST':config('DB_HOST'),
+            'PORT':'',
+        }
     }
-}
 
 
 # Password validation
