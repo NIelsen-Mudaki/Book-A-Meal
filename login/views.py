d8290fd8be50426a07f08731f53a286e5f4a5182
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-30 07:55:44 +0300

    added login.html file

--- login/views.py
+++ login/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    context = {
+        'title':'BOOK-A-MEAL | LOGIN PAGE'
+    }
+    return render(request, "login.html", context)
