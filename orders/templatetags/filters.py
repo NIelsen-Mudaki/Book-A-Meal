37dd8ee230cc10f7544ae3c71b2de9020ff1edc4
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-31 11:10:18 +0300

    add sub-total and grand total

--- orders/templatetags/filters.py
+++ orders/templatetags/filters.py
@@ -10,33 +10,10 @@ def currency(number):
 def product(number , number1):
     return number * number1
 
-@register.filter (name='is_in_cart')
-def is_in_cart(order):
-    keys = order.keys ()
-    for id in keys:
-        if int (id) == order.id:
-            return True
-    return False;
-
-
-@register.filter (name='quantity')
-def quantity(order):
-
-    for id in order:
-        if int (id) == order.id:
-            return order.get (id)
-    return 0;
-
-
-@register.filter (name='sub_total')
-def sub_total(order):
-    return order.price * quantity (order)
-
-
 @register.filter (name='grand_total')
 def grand_total(orders):
     sum = 0;
     for order in orders:
-        sum += sub_total (order)
+        sum += product (order)
 
     return sum
--- sales/templatetags/filters.py
+++ sales/templatetags/filters.py
@@ -10,25 +10,10 @@ def currency(number):
 def product(number , number1):
     return number * number1
 
-
-@register.filter (name='quantity')
-def quantity(order):
-
-    for id in order:
-        if int (id) == order.id:
-            return order.get (id)
-    return 0;
-
-
-@register.filter (name='sub_total')
-def sub_total(order):
-    return order.price * quantity (order)
-
-
 @register.filter (name='grand_total')
 def grand_total(orders):
     sum = 0;
     for order in orders:
-        sum += sub_total (order)
+        sum += product (order)
 
     return sum
