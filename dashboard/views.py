7a1c3743d0158b7a3e295b720b5c3df6817f37cc
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-01 11:38:38 +0300

    added dashboard:

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from customer.models import Customer
+from orders.models import Orders
 
 # Create your views here.
 def dashboard_view(request):
+    admins = Customer.objects.filter(is_Caterer=True).count()
+    clients = Customer.objects.filter(is_Customer=True).count()
+    orders = Orders.objects.all().count()
+
     context = {
         'title' : 'BOOK-A-MEAL | DASHBOARD',
+        'admins':admins,
+        'orders':orders,
+        'clients':clients
     }
     return render(request, "dashboard.html",context)
