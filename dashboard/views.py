251051fdb0de2c9cc8b0e7f216600ac0a40b7824
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-01 12:21:38 +0300

    added test html file

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,11 +7,15 @@ def dashboard_view(request):
     admins = Customer.objects.filter(is_Caterer=True).count()
     clients = Customer.objects.filter(is_Customer=True).count()
     orders = Orders.objects.all().count()
-
+    try:
+        items = Orders.objects.all().order_by('-id')[:5]
+    except:
+        items = 'noitem'
     context = {
         'title' : 'BOOK-A-MEAL | DASHBOARD',
         'admins':admins,
         'orders':orders,
-        'clients':clients
+        'clients':clients,
+        'items':items
     }
     return render(request, "dashboard.html",context)
