0d2c3c0c303d248e6220c9f4938fc631ba97153b
Francis Githae <francis.githae@student.moringaschool.com>
2022-04-01 11:25:26 +0300

    fix missing cookie bug

--- static/menujs/index.js
+++ static/menujs/index.js
@@ -1,11 +1,13 @@
-document.cookie='activedate=2022-04-06'
+// document.cookie='activedate=2022-04-06'
 $(()=>{
 
 
 $('#menudate').on('change',(event)=>{
   console.log('date changed');
   var selected_date=$('#menudate').val();
-  document.cookie=`activedate=${selected_date}`
+  document.cookie = "activedate= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+  document.cookie=`activedate=${selected_date}`;
+  localStorage.setItem('activedate',selected_date);
   $('#menudatepicker').submit()
 });
 
