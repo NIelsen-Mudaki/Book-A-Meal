29828366a03a31bcc035a49776302b271f9a606e
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-04-05 17:51:56 +0300

    added exception for edited cookie

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from webbrowser import get
 from django import views
 from django.urls import path, re_path
-from api.views import get_orders,get_menu,signup,create_order,get_customer_order,reset_password,login,getuser,signupnewslater
+from api.views import get_orders,get_menu,signup,create_order,get_customer_order,reset_password,login,getuser,signupnewslater,get_fiewmenu
 
 #urls
 urlpatterns = [
@@ -13,5 +13,6 @@ urlpatterns = [
     path('orders/create/',create_order),
     re_path('customer/(.*)/orders/',get_customer_order),
     path('reset/password',reset_password),
-    path('newsletter/',signupnewslater)
+    path('newsletter/',signupnewslater),
+    path('getfiewmenu/',get_fiewmenu)
 ]
