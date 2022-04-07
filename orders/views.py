2f5e766d09d984b0593ebbaa1865e90f760c6661
Francis Githae <francis.githae@student.moringaschool.com>
2022-04-07 10:50:05 +0300

    update admin orders view

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from customer.models import Customer
-from orders.models import Orders
+from orders.models import Order, Orders,OrderItem
 import datetime as dt
 
 # Create your views here.
 def orders(request):
     customer = request.user.id
     orders = Orders.get_all_orders()
+
+    order_items=OrderItem.objects.all()
+    print('You have the following order items')
+    for item in order_items:
+
+        # item['subtotal']=int(item.quantity * item.menu_id.price)
+        print(item.order.order_ref)
     
     print(orders)
-    return render(request,'orders.html',{'orders' : orders})
+    context={
+        'orders' : orders,
+        'orderitems':order_items
+    }
+    return render(request,'orders.html',context)
 
 def order_history(request):
     orders = Orders.get_all_orders()
@@ -29,6 +40,8 @@ def order_history(request):
         'error': error,
     }
     return render(request, 'order-history.html',context)
+
+
 
 # def search_results(request):
 #     if 'order_date' in request.GET and request.GET['order_date']:
